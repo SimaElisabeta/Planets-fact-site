2d50bedd54fc81c11eb8150b1873736e02a72935
SimaElisabeta <sima.elisabeta@gmail.com>
2023-05-05 17:50:32 +0300

    added PlanetType model and linked category to all Planets

--- plantes_fact_site/planets_app/models.py
+++ plantes_fact_site/planets_app/models.py
@@ -3,7 +3,28 @@ from django.core.validators import FileExtensionValidator
 
 
 # Create your models here.
+class PlanetType(models.Model):
+    TERESTRIAL_PLANET = "TP"
+    GAS_GIANT = "GGP"
+
+    PLANET_TYPES_CATEGORY = [
+        (TERESTRIAL_PLANET, "Terrestrial Planet"),
+        (GAS_GIANT, "Gas Giant")
+    ]
+
+    planet_category = models.CharField(max_length=3, choices=PLANET_TYPES_CATEGORY)
+
+    def __str__(self):
+        # tuple to dict conversion -> example: {'TP': 'Terrestrial Planet', 'GGP': 'Gas Giant'}
+        planet_category_dict = dict(self.PLANET_TYPES_CATEGORY)
+
+        # storing value, where self.planet_category represents the key for the value I want to store
+        category_full_name = planet_category_dict[self.planet_category]
+        return f"{category_full_name}"
+
+
 class Planet(models.Model):
+    category_id = models.ForeignKey(PlanetType, on_delete=models.CASCADE, default=1)
     name = models.CharField(max_length=50)
     overview = models.TextField()
     overview_source = models.URLField()
